249a77747b66f4015ca02fabae161cbd6c3e1f12
ahmadhany99 <ahmadhany1999@hotmail.com>
2022-11-21 16:41:48 -0500

    Added new functions

--- Main.py
+++ Main.py
@@ -1,5 +1,3 @@
-import queue as queue
-from CarClass import Car
 from Board import Board
 
 
@@ -12,44 +10,17 @@ def ReadingFile(file):
     return puzzles
 
 
-def printMatrix(board, puzzles):
-    ctr = 0
-    for i in range(6):
-        for x in range(6):
-            board.matrix[i][x] = puzzles[ctr]
-            ctr = ctr + 1
-
-
-def matrixform(board):
-    for row in board.matrix:
-        for col in row:
-            print(col, end=" ")
-        print(" ")
-
-
-def createCars(x):  # have to add the fuel once we figure it out
-    carList = []
-    count = 0
-    listOfKeys = list(x.keys())
-    for values in x.values():
-        if not values[1]:
-            carList.append(Car((listOfKeys[count]), "Vertical", values[2], values[0]))
-
-        else:
-            carList.append(Car((listOfKeys[count]), "Horizontal", values[2], values[0]))
-        count += 1
-    return carList
-
-
 puzzles = ReadingFile("sample-input.txt")
-board = Board()
-newCars = {}
-printMatrix(board, puzzles[1])
-matrixform(board)
+board = Board(puzzles[0])
+board.matrixform()
 print("\n")
 carsInfo = board.getCarsInfo()
-AllCars = createCars(carsInfo)
-boardCars=board.CreateNewCars(AllCars)
-trial=board.getCarName("A")
-# do the swap method
-# get the fuel
+AllCars = board.createCars()
+boardCars = board.CreateNewCars(AllCars)
+car2 = board.getCarName("M")
+board2 = board.moveDown(car2)
+trial = board2.getCarName("A")
+board3 = board2.moveRight(board2.getCarName("A"))
+board3.matrixform()
+for i in range(len(board3.cars)):
+    print(board3.cars[i].carName, end=" ")
